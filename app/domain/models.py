faf2793806d74136917522071608d9650d63e9ff
GuillermoLB <guillelb99@gmail.com>
2025-01-16 22:20:27 +0100

    Create models.

---
+++ app/domain/models.py
@@ -0,0 +1,40 @@
+from typing import Optional
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+Base = declarative_base()
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String, nullable=False)
+
+
+class Model(Base):
+    __tablename__ = 'models'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    llm: Mapped[str] = mapped_column(String, nullable=False)
+    max_length: Mapped[int] = mapped_column(Integer, nullable=False)
+    temperature: Mapped[float] = mapped_column(Float, nullable=False)
+    top_p: Mapped[float] = mapped_column(Float, nullable=False)
+
+
+class TextGeneration(Base):
+    __tablename__ = 'text_generations'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    prompt: Mapped[str] = mapped_column(String, nullable=False)
+    generated_text = Mapped[Optional[str]
+                            ] = mapped_column(String, nullable=True)
